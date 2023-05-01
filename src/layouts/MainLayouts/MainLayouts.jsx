47a6da777ff8a995a92abcdec1db6d5ee16ace63
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-05-01 21:30:53 +0600

    footer added

--- src/layouts/MainLayouts/MainLayouts.jsx
+++ src/layouts/MainLayouts/MainLayouts.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import MyNavbar from '../../components/Shared/MyNavbar/MyNavbar';
 import { Outlet } from 'react-router-dom';
+import MyFooter from '../../components/Shared/MyFooter/MyFooter';
 
 const MainLayouts = () => {
     return (
-        <div>
-            <div className="mynavbar">
+        <div className=''>
+            <div className="mynavbar ">
                 <MyNavbar></MyNavbar>
             </div>
-            <div className="outlet">
+            <div className="outlet my-container h-screen">
                 <Outlet></Outlet>
             </div>
+            <div className="footer">
+                <MyFooter></MyFooter>
+            </div>
+
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='my-container'>
+        <div className=''>
             This is a home
         </div>
     );
