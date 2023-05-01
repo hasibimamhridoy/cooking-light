0cd4baaedc2f7eb867ad9c43ef06dd66a69d045b
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-05-01 21:11:18 +0600

    navbar added

--- src/components/Shared/MyNavbar/MyNavbar.jsx
+++ src/components/Shared/MyNavbar/MyNavbar.jsx
@@ -1,11 +1,88 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const MyNavbar = () => {
-    return (
-        <div>
-            My Navbar
+  return (
+    <div className="">
+      <div className="navbar bg-base-100 shadow-md my-container">
+        <div className="navbar-start ">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <Link to="/">
+              <li>
+                <a>Home</a>
+              </li>
+            </Link>
+
+              <Link to="/blog">
+                <li>
+                  <a>Blog</a>
+                </li>
+              </Link>
+            </ul>
+          </div>
+          <Link to="/">
+            <a className=" normal-case text-xl cursor-pointer">Cooking Light</a>
+          </Link>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <Link to="/">
+              <li>
+                <a>Home</a>
+              </li>
+            </Link>
+
+            <Link to="/blog">
+              <li>
+                <a>Blog</a>
+              </li>
+            </Link>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">Profile</a>
+              </li>
+
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyNavbar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div className='my-container'>
             This is a home
         </div>
     );
