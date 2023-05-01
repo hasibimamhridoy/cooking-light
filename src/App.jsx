751fa2b1c268216966d70faa7721cf1731e2f83c
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-05-01 20:42:00 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+        <h1 className='bg-red-200'>Hello</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
